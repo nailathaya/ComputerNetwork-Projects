94b6b5daedcd9397253b25fdbf3513240f1439f8
nailathaya <athhayamumtaz@gmail.com>
2024-06-01 19:29:05 +0700

    client.py full command

--- client.py
+++ client.py
@@ -2,37 +2,42 @@ import socket
 import sys
 
 def http_client(server_host, server_port, path):
-    # Create a socket object
+    # Membuat socket objek baru
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect to the server
+    # Memulai koneksi dengan server
     client_socket.connect((server_host, server_port))
 
-    # Send HTTP GET request
+    # Mengirim HTTP GET Request 
     request = f"GET {path} HTTP/1.1\r\nHost: {server_host}\r\nConnection: close\r\n\r\n"
+
+    #Mengirim request yang di decode ke dalam bytes ke server
     client_socket.sendall(request.encode())
 
-    # Receive response from the server
-    response = b""
-    while True:
-        part = client_socket.recv(1024)
-        if not part:
-            break
-        response += part
+    # Menerima respons dari server (permintaan diterima atau file path tidak ditemukan.)
+    response = b""                           # Menginisialisasi variabel response dengan string kosong berupa bytes
+    while True:                              # Melakukan looping
+        part = client_socket.recv(1024)      # Menerima respons dari server
+        if not part:                         # Jika respons yang diterima bukan respons
+            break                            # bukan respons dari server, maka loop berhenti
+        response += part                     # tambahkan part dalam bentuk bytes tersebut ke variabel response
 
-    # Close the connection
+    # Menutup koneksi dengan server
     client_socket.close()
 
-    # Print the response
+    # Mengoutputkan response dalam bentuk string
     print(response.decode('utf-8'))
 
-if _name_ == "_main_":
-    if len(sys.argv) != 4:
-        print("Usage: python http_client.py <server_host> <server_port> <path>")
+if __name__ == "_main_":
+    # Permisalan panjang argumen (termasuk nama script, contoh: "client.py") 
+    # tidak sepanjang 4, maka beri pemberitahuan cara penggunaan yang benar.
+    if len(sys.argv) != 4: 
+        print("Usage: py client.py <server_host> <server_port> <path>")
         sys.exit(1)
 
-    server_host = sys.argv[1]
-    server_port = int(sys.argv[2])
-    path = sys.argv[3]
+    server_host = sys.argv[1]          # Inisialisasi argumen 1 = <server host> ke dalam server_host
+    server_port = int(sys.argv[2])     # Inisialisasi argumen 2 = <server port> ke dalam server_port
+    path = sys.argv[3]                 # Inisialisasi argumen 3 = <path> ke dalam path
 
+    # Memanggil fungsi http client dengan argumen server host, server port, dan path yang akan di request ke server
     http_client(server_host, server_port, path)
